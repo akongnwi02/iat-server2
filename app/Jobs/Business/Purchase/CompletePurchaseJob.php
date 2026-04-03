e867b3f4eca799fbbfec5c6df236a291c312e2a6
Devert <akongnwi02@gmail.com>
2026-04-03 20:43:09 +0200

    Fix pusher event not found

--- app/Jobs/Business/Purchase/CompletePurchaseJob.php
+++ app/Jobs/Business/Purchase/CompletePurchaseJob.php
@@ -9,12 +9,8 @@
 namespace App\Jobs\Business\Purchase;
 
 use App\Exceptions\Api\ServerErrorException;
-use App\Http\Resources\Api\TransactionResource;
 use App\Jobs\Job;
 use App\Models\Transaction\Transaction;
-use App\Repositories\Backend\Movement\MovementRepository;
-use App\Services\Constants\BusinessErrorCodes;
-use Pusher\Pusher;
 
 class CompletePurchaseJob extends Job
 {
@@ -52,12 +48,11 @@ class CompletePurchaseJob extends Job
     
     /**
      * @throws ServerErrorException
-     * @throws \Pusher\PusherException
      */
     public function handle()
     {
         
-        \Log::info("{$this->getJobName()}: Transaction is terminated. Sending event to pusher...", [
+        \Log::info("{$this->getJobName()}: Transaction is terminated.", [
             'transaction.status'       => $this->transaction->status,
             'transaction.asset'        => $this->transaction->asset,
             'transaction.code'         => $this->transaction->code,
@@ -69,26 +64,7 @@ class CompletePurchaseJob extends Job
             'transaction.error_code'   => $this->transaction->error_code,
             'transaction.service_code' => $this->transaction->service_code,
         ]);
-        
-        $pusher = new Pusher(
-            config('broadcasting.connections.pusher.key'),
-            config('broadcasting.connections.pusher.secret'),
-            config('broadcasting.connections.pusher.app_id'),
-            config('broadcasting.connections.pusher.options')
-        );
-        
-        $triggered = $pusher->trigger(
-            $this->transaction->uuid,
-            config('broadcasting.connections.pusher.events.transaction_complete'),
-            new TransactionResource($this->transaction)
-        );
-        
-        if (!$triggered) {
-            \Log::error("{$this->getJobName()}: Error encountered while sending event to pusher");
-            throw new ServerErrorException(BusinessErrorCodes::GENERAL_CODE, 'Error connection to the pusher server');
-        }
-        
-        \Log::info("{$this->getJobName()}: Event sent to pusher successfully");
+
     }
     
     public function getJobName()
